2369eab1a7eabf80265b54d43dec0744dcf6f05b
Aleksandr Molodykh <caneyok@gmail.com>
2022-03-10 14:01:00 +0300

    Добавлено удаление элементов без содержимого

--- js/html-ad-generator.js
+++ js/html-ad-generator.js
@@ -1,4 +1,11 @@
 import { adPool } from './ad-generator.js';
+const СardType = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель'
+};
 
 const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 const popupList = [];
@@ -6,34 +13,42 @@ const popupList = [];
 const createPopup = (cardInfo) => {
   const newCard = cardTemplate.cloneNode(true);
   const {title, address, price, type, rooms, guests, checkin, checkout, features, description, photos} = cardInfo.offer;
+
   newCard.querySelector('.popup__title').textContent = title;
   newCard.querySelector('.popup__text--address').textContent = `Координаты: ${address}`;
   newCard.querySelector('.popup__text--price').textContent = `${price} ₽/ночь`;
-  const cardType = {
-    flat: 'Квартира',
-    bungalow: 'Бунгало',
-    house: 'Дом',
-    palace: 'Дворец',
-    hotel: 'Отель'
-  };
-  newCard.querySelector('.popup__type').textContent = cardType[type];
+  newCard.querySelector('.popup__type').textContent = СardType[type];
   newCard.querySelector('.popup__text--capacity').textContent = `${rooms} комнаты для ${guests} гостей`;
   newCard.querySelector('.popup__text--time').textContent = `Заезд после ${checkin}, выезд до ${checkout}`;
   const featureList = newCard.querySelectorAll('.popup__feature');
-  featureList.forEach((el) => {
-    const flatHasFeature = features.some((feature) => el.classList.contains(`popup__feature--${feature}`));
-    if (!flatHasFeature) {
-      el.remove();
-    }
-  });
-  newCard.querySelector('.popup__description').textContent = description;
+  if (features.length !== 0) {
+    featureList.forEach((el) => {
+      const flatHasFeature = features.some((feature) => el.classList.contains(`popup__feature--${feature}`));
+      if (!flatHasFeature) {
+        el.remove();
+      }
+    });
+  } else {
+    newCard.querySelector('.popup__features').remove();
+  }
+  const popupDescription = newCard.querySelector('.popup__description');
+  if (description.length !== 0) {
+    popupDescription.textContent = description;
+  } else {
+    popupDescription.remove();
+  }
   const photoList = newCard.querySelector('.popup__photos');
-  photos.forEach((photo) => {
-    const newPhoto = photoList.querySelector('.popup__photo').cloneNode();
-    newPhoto.src = photo;
-    photoList.appendChild(newPhoto);
-  });
-  photoList.querySelector('.popup__photo').remove();
+  if (photos.length !== 0) {
+    const photoTemplate = photoList.querySelector('.popup__photo');
+    photos.forEach((photo) => {
+      const newPhoto = photoTemplate.cloneNode();
+      newPhoto.src = photo;
+      photoList.appendChild(newPhoto);
+    });
+    photoTemplate.remove();
+  } else {
+    photoList.remove();
+  }
   newCard.querySelector('.popup__avatar').src = cardInfo.author.avatar;
   return newCard;
 };
