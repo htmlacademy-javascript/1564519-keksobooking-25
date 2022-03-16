8a621401ddf1fa0137652b4ad56a60914051e385
Aleksandr Molodykh <caneyok@gmail.com>
2022-03-16 23:10:13 +0300

    Улучшена валидация: сообщения об ошибках стали яснее

--- js/form-validator.js
+++ js/form-validator.js
@@ -1,10 +1,17 @@
 const form = document.querySelector('.ad-form');
 const MinPricePerNight = {
-  flat: 1000,
-  bungalow: 0,
-  house: 5000,
-  palace: 10000,
-  hotel: 3000
+  FLAT: 1000,
+  BUNGALOW: 0,
+  HOUSE: 5000,
+  PALACE: 10000,
+  HOTEL: 3000
+};
+const ErrorHouseNames = {
+  FLAT: 'квартире',
+  BUNGALOW: 'бунгало',
+  HOUSE: 'доме',
+  PALACE: 'дворце',
+  HOTEL: 'отеле'
 };
 const GuestOptions = {
   1: [1],
@@ -12,30 +19,36 @@ const GuestOptions = {
   3: [1, 2, 3],
   100: [0]
 };
+const TITLEMINLENGTH = 30;
+const TITLEMAXLENGTH = 100;
+const MAXPRICE = 100000;
 
 const pristine = new Pristine(form, {
-  classTo: 'notice',
-  errorTextParent: 'notice',
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
   errorTextClass: 'ad-form__error-text'
 }, true);
 
 const titleInput = document.querySelector('#title');
-function validateTitleInput (value) {
-  return value.length && value.match(/^[A-Za-zА-Яа-яЁё0-9]{30,100}$/);
-}
-pristine.addValidator(titleInput, validateTitleInput, 'Заголовок объявления должен состоять из букв русского или латинского алфавита и цифр. Длина заголовка должна быть больше 30 и меньше 100 символов.');
+const validateTitleLanguage = (value) => value.match(/^[A-Za-zА-Яа-яЁё0-9]*$/);
+const validateTitleLength = (value) => (value.length >= TITLEMINLENGTH && value.length <= TITLEMAXLENGTH) || value.length === 0;
+const validateTitle = (value) => value.length;
+pristine.addValidator(titleInput, validateTitleLanguage, 'Заголовок объявления должен состоять из букв русского или латинского алфавита и цифр.');
+pristine.addValidator(titleInput, validateTitleLength, `Длина заголовка должна быть больше ${TITLEMINLENGTH} и меньше ${TITLEMAXLENGTH} символов.`);
+pristine.addValidator(titleInput, validateTitle, 'Поле является обязательным.');
 
 const adType = document.querySelector('#type');
 const priceInput = document.querySelector('#price');
-function validatePriceInput (value) {
-  return value.length && Number(value) > MinPricePerNight[adType.value] && Number(value) < 100000;
-}
-function getPriceErrorMessage () {
-  return `Цена за ночь должна быть больше ${MinPricePerNight[adType.value]} и меньше 100000`;
-}
-pristine.addValidator(priceInput, validatePriceInput, getPriceErrorMessage);
+const validateMinPriceInput = (value) => Number(value) > MinPricePerNight[adType.value.toUpperCase()] || Number(value) === 0;
+const validateMaxPriceInput = (value) => Number(value) <= MAXPRICE;
+const validatePriceInput = (value) => value.length;
+const getMinPriceErrorMessage = () => `Ночь в ${ErrorHouseNames[adType.value.toUpperCase()]} не может стоить меньше ${MinPricePerNight[adType.value.toUpperCase()]}`;
+const getMaxPriceErrorMessage = () => `Цена за ночь не должна превышать ${MAXPRICE}`;
+pristine.addValidator(priceInput, validateMinPriceInput, getMinPriceErrorMessage);
+pristine.addValidator(priceInput, validateMaxPriceInput, getMaxPriceErrorMessage);
+pristine.addValidator(priceInput, validatePriceInput, 'Поле является обязательным.');
 adType.addEventListener('change', () => {
-  priceInput.placeholder = `${MinPricePerNight[adType.value] + 4000}`;
+  priceInput.placeholder = `${MinPricePerNight[adType.value.toUpperCase()] + 4000}`;
   pristine.validate(priceInput);
 });
 
@@ -50,17 +63,31 @@ timeOutField.addEventListener('change', () => {
 
 const roomInput = document.querySelector('#room_number');
 const guestInput = document.querySelector('#capacity');
-function validateGuestInput () {
-  return GuestOptions[roomInput.value].includes(Number(guestInput.value));
-}
-function getGuestErrorMessage () {
+const validateGuestInput = () => GuestOptions[roomInput.value].includes(Number(guestInput.value));
+const getGuestErrorMessage = () => {
   const selectedRooms = roomInput.querySelector('option:checked');
   const selectedGuests = guestInput.querySelector('option:checked');
-  return `${selectedRooms.textContent} не подходит ${selectedGuests.textContent}`;
-}
+  if (roomInput.value === '100' && guestInput.value !== '0') {
+    return `${selectedRooms.textContent} ${guestInput.querySelector('option[value="0"]').textContent}`;
+  }
+  return `${selectedRooms.textContent} ${roomInput.value === '1' ? 'не подходит' : 'не подходят'} ${selectedGuests.textContent}`;
+};
+const getRoomErrorMessage = () => {
+  const selectedRooms = roomInput.querySelector('option:checked');
+  const selectedGuests = guestInput.querySelector('option:checked');
+  if (roomInput.value === '100' && guestInput.value !== '0') {
+    return `${selectedRooms.textContent} ${guestInput.querySelector('option[value="0"]').textContent}`;
+  }
+  return `${selectedGuests.textContent} ${roomInput.value === '1' ? 'нужна' : 'нужно'} как минимум ${selectedRooms.textContent}`;
+};
 pristine.addValidator(guestInput, validateGuestInput, getGuestErrorMessage);
-pristine.addValidator(roomInput, validateGuestInput, getGuestErrorMessage);
-
+pristine.addValidator(roomInput, validateGuestInput, getRoomErrorMessage);
+roomInput.addEventListener('change', () => {
+  pristine.validate(guestInput);
+});
+guestInput.addEventListener('change', () => {
+  pristine.validate(roomInput);
+});
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
