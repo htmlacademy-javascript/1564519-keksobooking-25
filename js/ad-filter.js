9735ff722a3b8bc831eef94188dc689564484bf5
Aleksandr Molodykh <caneyok@gmail.com>
2022-04-02 17:09:42 +0300

    debounce теперь работает

--- js/ad-filter.js
+++ js/ad-filter.js
@@ -7,26 +7,27 @@ const RERENDER_DELAY = 500;
 const mapFiltersContainer = document.querySelectorAll('.map__filter, .map__checkbox');
 
 const turnSimilarAdPinFilterOn = (data) => {
+  const filterChange = debounce((filterInput) => {
+    if (filterInput.type === 'checkbox') {
+      switchFeatureFilter(adOffer['features'], filterInput.value);
+    } else {
+      adOffer[`${filterInput.id.split('-')[1]}`] = filterInput.value;
+    }
+    const dataForPins = data.filter((ad) => isAdSimilar(adOffer, ad['offer'])).slice(0, SIMILAR_ADS_COUNT);
+    const pins = document.querySelectorAll('.leaflet-marker-icon');
+    const popup = document.querySelector('.leaflet-popup-content-wrapper');
+    if (popup) {
+      popup.remove();
+    }
+    pins.forEach((el, i) => {
+      if (i !== 0) {
+        el.remove();
+      }
+    });
+    renderAdPins(dataForPins);
+  }, RERENDER_DELAY);
   mapFiltersContainer.forEach((filterInput) => {
-    filterInput.addEventListener('change', debounce(() => {
-      if (filterInput.type === 'checkbox') {
-        switchFeatureFilter(adOffer['features'], filterInput.value);
-      } else {
-        adOffer[`${filterInput.id.split('-')[1]}`] = filterInput.value;
-      }
-      const dataForPins = data.filter((ad) => isAdSimilar(adOffer, ad['offer'])).slice(0, SIMILAR_ADS_COUNT);
-      const pins = document.querySelectorAll('.leaflet-marker-icon');
-      const popup = document.querySelector('.leaflet-popup-content-wrapper');
-      if (popup) {
-        popup.remove();
-      }
-      pins.forEach((el, i) => {
-        if (i !== 0) {
-          el.remove();
-        }
-      });
-      renderAdPins(dataForPins);
-    }, RERENDER_DELAY));
+    filterInput.addEventListener('change', () => filterChange(filterInput));
   });
 };
 
